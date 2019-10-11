6e07e58429a0bb2d6ba926885e9be496ef95faaa
Ark1700 <Ark1700@yandex.ru>
2019-10-11 14:35:26 +0300

    Правки

--- js/main.js
+++ js/main.js
@@ -211,6 +211,7 @@ var activateMap = function (evt) {
     disableAllInputs(false);
     renderMap(ads, map);
     setInputLocation();
+    validtionForm();
 
     var mapPins = map.querySelectorAll('.map__pin:not(.map__pin--main)');
     for (var i = 0; i < mapPins.length; i++) {
@@ -225,80 +226,89 @@ mainPin.addEventListener('mousedown', activateMap);
 
 mainPin.addEventListener('keydown', activateMap);
 
-var adForm = document.querySelector('.ad-form');
-var guestsNumberInput = adForm.querySelector('#capacity');
-var roomsNumberInput = adForm.querySelector('#room_number');
+var validtionForm = function () {
+  var adForm = document.querySelector('.ad-form');
+  var guestsNumberInput = adForm.querySelector('#capacity');
+  var roomsNumberInput = adForm.querySelector('#room_number');
 
-adForm.addEventListener('input', function () {
-  if (guestsNumberInput.value > roomsNumberInput.value) {
-    guestsNumberInput.setCustomValidity('Кол-во гостей не должно превышать кол-ва комнат');
-  } else {
-    guestsNumberInput.setCustomValidity('');
-  }
-});
-
-// validation
-var adTitle = adForm.querySelector('input[name="title"]');
-var adPrice = adForm.querySelector('input[name="price"]');
-var adType = adForm.querySelector('select[name="type"]');
-var adTimeIn = adForm.querySelector('select[name="timein"]');
-var adTimeOut = adForm.querySelector('select[name="timeout"]');
-var minTypePrice = [
-  ['bungalo', 'flat', 'house', 'palace'],
-  [0, 1000, 5000, 10000]
-];
-
-var getMinTypePrice = function (type) {
-  var result;
-  for (var i = 0; i < minTypePrice[0].length; i++) {
-    if (type === minTypePrice[0][i]) {
-      result = minTypePrice[1][i];
+  adForm.addEventListener('input', function () {
+    if (guestsNumberInput.value > roomsNumberInput.value) {
+      guestsNumberInput.setCustomValidity('Кол-во гостей не должно превышать кол-ва комнат');
+    } else {
+      guestsNumberInput.setCustomValidity('');
     }
-  }
-  return result;
+  });
+
+  var adTitle = adForm.querySelector('input[name="title"]');
+  var adPrice = adForm.querySelector('input[name="price"]');
+  var adType = adForm.querySelector('select[name="type"]');
+  var adTimeIn = adForm.querySelector('select[name="timein"]');
+  var adTimeOut = adForm.querySelector('select[name="timeout"]');
+  var minTypePrice = [
+    ['bungalo', 'flat', 'house', 'palace'],
+    [0, 1000, 5000, 10000]
+  ];
+
+  var getMinTypePrice = function (type) {
+    var result;
+    for (var i = 0; i < minTypePrice[0].length; i++) {
+      if (type === minTypePrice[0][i]) {
+        result = minTypePrice[1][i];
+      }
+    }
+    return result;
+  };
+
+  adTitle.addEventListener('invalid', function () {
+    switch (true) {
+      case adTitle.validity.tooShort:
+        adTitle.setCustomValidity('Заголовок объявления должeн состоять минимум из 30 символов');
+        break;
+      case adTitle.validity.tooLong:
+        adTitle.setCustomValidity('Заголовок объявления не должен превышать 100 символов');
+        break;
+      case adTitle.validity.valueMissing:
+        adTitle.setCustomValidity('Обязательное поле');
+        break;
+      default:
+        adTitle.setCustomValidity('');
+    }
+  });
+
+  adPrice.addEventListener('invalid', function () {
+    switch (true) {
+      case adTitle.validity.tooLong:
+        adTitle.setCustomValidity('Цена не должна превышать 1 000 0000 руб.');
+        break;
+      case adTitle.validity.valueMissing:
+        adTitle.setCustomValidity('Обязательное поле');
+        break;
+      default:
+        adTitle.setCustomValidity('');
+    }
+  });
+
+  adPrice.addEventListener('input', function (evt) {
+    var target = evt.target;
+    adType = adForm.querySelector('select[name="type"]');
+    if (target.value < getMinTypePrice(adType.value)) {
+      target.setCustomValidity('Минимальная "Цена за ночь" для ' + adType.options[adType.selectedIndex].innerHTML + ' - ' + getMinTypePrice(adType.value) + ' руб.');
+      return;
+    }
+    target.setCustomValidity('');
+  });
+
+  adType.addEventListener('input', function () {
+    adPrice.min = getMinTypePrice(adType.value);
+    adPrice.placeholder = getMinTypePrice(adType.value);
+  });
+
+  adTimeIn.addEventListener('input', function () {
+    adTimeOut.value = adTimeIn.value;
+  });
+
+  adTimeOut.addEventListener('input', function () {
+    adTimeIn.value = adTimeOut.value;
+  });
 };
 
-adTitle.addEventListener('invalid', function () {
-  if (adTitle.validity.tooShort) {
-    adTitle.setCustomValidity('Заголовок объявления должeн состоять минимум из 30 символов');
-  } else if (adTitle.validity.tooLong) {
-    adTitle.setCustomValidity('Заголовок объявления не должен превышать 100 символов');
-  } else if (adTitle.validity.valueMissing) {
-    adTitle.setCustomValidity('Обязательное поле');
-  } else {
-    adTitle.setCustomValidity('');
-  }
-});
-
-adPrice.addEventListener('invalid', function () {
-  if (adTitle.validity.tooLong) {
-    adTitle.setCustomValidity('Заголовок объявления не должен превышать 100 символов');
-  } else if (adTitle.validity.valueMissing) {
-    adTitle.setCustomValidity('Обязательное поле');
-  } else {
-    adTitle.setCustomValidity('');
-  }
-});
-
-adPrice.addEventListener('input', function (evt) {
-  var target = evt.target;
-  adType = adForm.querySelector('select[name="type"]');
-  if (target.value < getMinTypePrice(adType.value)) {
-    target.setCustomValidity('Минимальная "Цена за ночь" для ' + adType.options[adType.selectedIndex].innerHTML + ' - ' + getMinTypePrice(adType.value) + ' руб.');
-  } else {
-    target.setCustomValidity('');
-  }
-});
-
-adType.addEventListener('input', function () {
-  adPrice.min = getMinTypePrice(adType.value);
-  adPrice.placeholder = getMinTypePrice(adType.value);
-});
-
-adTimeIn.addEventListener('input', function () {
-  adTimeOut.value = adTimeIn.value;
-});
-
-adTimeOut.addEventListener('input', function () {
-  adTimeIn.value = adTimeOut.value;
-});
