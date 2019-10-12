9530497619f81120e193f5b9071395ec4b6242b4
Ark1700 <Ark1700@yandex.ru>
2019-10-12 03:10:26 +0300

    Разделил проект на модули

---
+++ js/map.js
@@ -0,0 +1,48 @@
+'use strict';
+
+(function () {
+  var renderMap = function (pin, mapWindow) {
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < pin.length; i++) {
+      fragment.appendChild(window.pin.renderPin(pin[i]));
+    }
+    mapWindow.appendChild(fragment);
+  };
+
+  var locationInput = document.querySelector('#address');
+  var mainPin = document.querySelector('.map__pin--main');
+  var MAIN_PIN_WIDTH = 65;
+  var MAIN_PIN_HEIGHT = 87;
+
+  var setInputLocation = function () {
+    var mainPinStyle = getComputedStyle(mainPin);
+    var coordinateX = Math.round(Number(mainPinStyle.left.slice(0, -2), 10) + MAIN_PIN_WIDTH / 2);
+    var coordinateY = Math.round(Number(mainPinStyle.top.slice(0, -2), 10) + MAIN_PIN_HEIGHT);
+    locationInput.value = coordinateX + ', ' + coordinateY;
+  };
+
+  var activateMap = function (evt) {
+    if (evt.type === 'mousedown' || evt.code === 'Enter') {
+      mainPin.removeEventListener('mousedown', activateMap);
+      mainPin.removeEventListener('keydown', activateMap);
+      document.querySelector('.map').classList.remove('map--faded');
+      document.querySelector('.ad-form').classList.remove('ad-form--disabled');
+      window.form.disableAllInputs(false);
+      renderMap(window.ads, window.map);
+      setInputLocation();
+      window.form.validtionForm();
+
+      var mapPins = window.map.querySelectorAll('.map__pin:not(.map__pin--main)');
+      for (var i = 0; i < mapPins.length; i++) {
+        window.pin.addingPinHandler(mapPins, i);
+      }
+    }
+  };
+
+  mainPin.addEventListener('mousedown', activateMap);
+
+  mainPin.addEventListener('keydown', activateMap);
+
+  window.form.disableAllInputs();
+
+})();
---
+++ js/util.js
@@ -0,0 +1,26 @@
+'use strict';
+(function () {
+  var getRandomFromArr = function (arr, count) {
+    if (count === -1) {
+      return arr[Math.floor(Math.random() * arr.length)];
+    } else {
+      var clone = arr;
+      var result = [];
+      for (var i = 0; i < count || i < arr.length; i++) {
+        var index = Math.floor(Math.random());
+        result.push(clone[index * clone.length]);
+        clone.splice(index, 1);
+      }
+      return result;
+    }
+  };
+
+  window.PIN_WIDTH = 50;
+  window.PIN_HEIGHT = 70;
+
+  window.map = document.querySelector('.map__pins');
+
+  window.util = {
+    getRandomFromArr: getRandomFromArr,
+  };
+})();
