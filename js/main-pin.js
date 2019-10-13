ec6c228eb417fbd8d0ae62db27dbd0fae638aae1
Ark1700 <Ark1700@yandex.ru>
2019-10-13 15:43:41 +0300

    Добавлено перемещение главной метки

---
+++ js/main-pin.js
@@ -0,0 +1,80 @@
+'use strict';
+(function () {
+
+  var locationInput = document.querySelector('#address');
+  var mainPin = document.querySelector('.map__pin--main');
+  var MAIN_PIN_WIDTH = 65;
+  var MAIN_PIN_HEIGHT = 87;
+
+  var setInputLocation = function () {
+    var mainPinStyle = getComputedStyle(mainPin);
+    var coordinateX = Math.round(Number(mainPinStyle.left.slice(0, -2), 10) + MAIN_PIN_WIDTH / 2);
+    var coordinateY = Math.round(Number(mainPinStyle.top.slice(0, -2), 10) + MAIN_PIN_HEIGHT);
+    locationInput.value = coordinateX + ', ' + coordinateY;
+  };
+
+  // drag dialog
+
+  mainPin.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+
+      var Y_MIN = 129;
+      var Y_MAX = 601;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      if (
+        mainPin.offsetLeft - shift.x > -MAIN_PIN_WIDTH / 2 && // x min
+        mainPin.offsetLeft - shift.x < window.map.clientWidth - MAIN_PIN_WIDTH / 2 && // x max
+        mainPin.offsetTop - shift.y > Y_MIN - MAIN_PIN_HEIGHT && // y min
+        mainPin.offsetTop - shift.y < Y_MAX - MAIN_PIN_HEIGHT // y max
+      ) {
+        mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
+        mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
+        setInputLocation();
+      }
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (dragEvt) {
+          dragEvt.preventDefault();
+          mainPin.removeEventListener('click', onClickPreventDefault);
+        };
+        mainPin.addEventListener('click', onClickPreventDefault);
+      }
+
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
+  window.mainPin = {
+    setInputLocation: setInputLocation,
+  };
+})();
--- js/map.js
+++ js/map.js
@@ -9,17 +9,7 @@
     mapWindow.appendChild(fragment);
   };
 
-  var locationInput = document.querySelector('#address');
   var mainPin = document.querySelector('.map__pin--main');
-  var MAIN_PIN_WIDTH = 65;
-  var MAIN_PIN_HEIGHT = 87;
-
-  var setInputLocation = function () {
-    var mainPinStyle = getComputedStyle(mainPin);
-    var coordinateX = Math.round(Number(mainPinStyle.left.slice(0, -2), 10) + MAIN_PIN_WIDTH / 2);
-    var coordinateY = Math.round(Number(mainPinStyle.top.slice(0, -2), 10) + MAIN_PIN_HEIGHT);
-    locationInput.value = coordinateX + ', ' + coordinateY;
-  };
 
   var activateMap = function (evt) {
     if ((evt.type === 'mousedown' || evt.code === 'Enter') && window.ads) {
@@ -29,7 +19,7 @@
       document.querySelector('.ad-form').classList.remove('ad-form--disabled');
       window.form.disableAllInputs(false);
       renderMap(window.ads, window.map);
-      setInputLocation();
+      window.mainPin.setInputLocation();
       window.form.validtionForm();
 
       var mapPins = window.map.querySelectorAll('.map__pin:not(.map__pin--main)');
@@ -40,9 +30,7 @@
   };
 
   mainPin.addEventListener('mousedown', activateMap);
-
   mainPin.addEventListener('keydown', activateMap);
 
   window.form.disableAllInputs();
-
 })();
